1dfb29f7c589a55eaf3cb60282bc9cbe3afca0d8
Hannele Valtanen <hannele.valtanen@iki.fi>
2022-01-13 12:37:41 +0200

    Add React components

---
+++ react-components/src/components/stencil-generated/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'stencil-components';
+
+import { defineCustomElements } from 'stencil-components/loader';
+
+defineCustomElements();
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
+export const RdsButton = /*@__PURE__*/createReactComponent<JSX.RdsButton, HTMLRdsButtonElement>('rds-button');
+export const RdsDialog = /*@__PURE__*/createReactComponent<JSX.RdsDialog, HTMLRdsDialogElement>('rds-dialog');
