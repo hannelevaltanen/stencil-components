4132cea0474ad801e673832c215614e6fcdbe8f0
Hannele Valtanen <hannele.valtanen@iki.fi>
2022-01-17 14:19:35 +0200

    Update React components

--- react-components/src/components/stencil-generated/index.ts
+++ react-components/src/components/stencil-generated/index.ts
@@ -11,3 +11,5 @@ defineCustomElements();
 export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
 export const RdsButton = /*@__PURE__*/createReactComponent<JSX.RdsButton, HTMLRdsButtonElement>('rds-button');
 export const RdsDialog = /*@__PURE__*/createReactComponent<JSX.RdsDialog, HTMLRdsDialogElement>('rds-dialog');
+export const RdsRadio = /*@__PURE__*/createReactComponent<JSX.RdsRadio, HTMLRdsRadioElement>('rds-radio');
+export const RdsRadioGroup = /*@__PURE__*/createReactComponent<JSX.RdsRadioGroup, HTMLRdsRadioGroupElement>('rds-radio-group');
